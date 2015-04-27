6a5fa77916bff7f7315feea89d771b3252fe58db
erikodiony <erikodiony@live.com>
2015-04-27 10:08:03 +0700

    Ori

--- Level/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host="localhost";
-	$user="root";
-	$password="";	
-	$koneksi=mysql_connect($host,$user,$password) or 
-	die("Gagal koneksi mas bro..!");
-	mysql_select_db("data");
-?>
--- Level/index.php
+++ Level/index.php
@@ -1,7 +1,29 @@
-<form method="post" action="login.php" class="form-horizontal">
-	<label class="username">Username</label>
-	<input type="text" name="username" placeholder="Username Anda">
-	<label class="password">Password</label>    	
-	<input type="password" name="pass" placeholder="Password">
-	<button type="submit" name="submit" class="btn">Login</button>
+<html>
+<head>
+<title>-=Data Buku Perpustakaan=-</title>
+</head>
+<body>
+<h2>./Masuk</h2>
+<form action="Masuk.php" method="post">
+<table><tr>
+<td>Username</td>
+<td><input type="text" name="user" size="20"></td>
+</tr><tr>
+<td>Password</td>
+<td><input type="password" name="pass" size="20"></td>
+</tr><tr>
+<td>Level</td>
+<td><select name="level">
+  <option value="admin">Admin</option>
+  <option value="user">User</option>
+</select></td>
+</tr>
+<tr>
+<tr><td><a href="Daftar.php" style="text-decoration:none">Belum punya akun ?</a></td></tr>
+<tr><td><a href="LupaGanti.php" style="text-decoration:none">Lupa / Ganti Password ?</a></td></tr>
+<td><br><input type="submit" name="proses" value="Masuk"></td>
+</tr>
+</table>
 </form>
+</body>
+</html>
